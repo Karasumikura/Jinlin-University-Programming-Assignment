31ec909a22e6ae59fbdfcb6d0e2dd0a35f6f755c
Kara Wang <tdie200538@gmail.com>
2024-11-19 17:07:11 +0800

    Add files via upload

---
+++ 程序设计基础/04/3(modified version).c
@@ -0,0 +1,57 @@
+//'c'类数字可以直接用‘d’类表示，注意表示后减去48
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define SIZE 10
+void bdd(int a[], int n) {
+	int j,ls;
+	for (int i = 1; i < n; i++) {
+		ls = a[i];
+		for (j = i - 1; j >= 0 && a[j] > ls; j--) {
+			a[j + 1] = a[j];
+		}
+		a[j + 1] = ls;
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d\n", a[i]);
+	}
+}
+void taffy(int a[],int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - 1 - i; j++) {
+			if (a[j] > a[j + 1]) {
+				int t = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = t;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d\n", a[i]);
+	}
+}
+
+void xrr(int a[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (a[i] > a[j]) {
+				int t = a[i];
+				a[i] = a[j];
+				a[j] = t;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d\n", a[i]);
+	}
+}
+
+int main() {
+	int a[SIZE];
+	for (int i = 0; i < SIZE; i++) {
+		scanf("%d", &a[i]);
+	}
+	bdd(a, SIZE);
+	taffy(a, SIZE);
+	xrr(a, SIZE);
+	return 0;
+}
