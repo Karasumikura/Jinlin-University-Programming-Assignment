e4ac733d9610ade95702a2e124c257923db5550c
Kara Wang <tdie200538@gmail.com>
2024-12-08 18:28:56 +0800

    Create 1.c

---
+++ 程序设计基础/07/1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int c(int m, int n) {
+	if (n < 0) return 0;
+	if(n == 0)  return 1;
+	if (n == 1)  return m;
+	if (m < 2 * n) return c(m, m - n);
+	if (m >= 2 * n) return c(m - 1, n - 1) + c(m - 1, n);
+}
+int main() {
+	int m, n;
+	scanf("%d %d", &m, &n);
+	printf("%d", c(m, n));
+}
